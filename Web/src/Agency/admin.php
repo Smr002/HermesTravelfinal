09b143c96a8d0102bae1ffee8e25c71edb5900b0
Ermin Lilaj <ermin.lilaj04@gmail.com>
2024-05-07 21:54:18 +0200

    implemented adminHeader&removed tours/reviews

--- Web/src/Agency/admin.php
+++ Web/src/Agency/admin.php
@@ -31,44 +31,7 @@
     <!-- End Header -->
 
     <!-- Sidebar -->
-    <aside id="sidebar">
-        <div class="sidebar-title">
-            <div class="sidebar-brand">
-                <span class="material-icons-outlined">menu</span>MENU
-            </div>
-        </div>
-        <ul class="sidebar-list">
-            <li class="sidebar-list-item">
-                <span class="material-icons-outlined">dashboard</span>Dashboard
-            
-            </li>
-            <li class="sidebar-list-item">
-                <a href="countries.php">
-                <span class="material-icons-outlined">flag</span>Countries
-                </a>
-            </li>
-            <li class="sidebar-list-item">
-                <a href="destinations-admin.php">
-                <span class="material-icons-outlined">place</span>Destinations
-                </a>
-            </li>
-            <li class="sidebar-list-item">
-                <a href="clients-admin.php">
-                <span class="material-icons-outlined">people</span>Clients
-                </a>
-            </li>
-            <li class="sidebar-list-item">
-                <a href="sales.php">
-                <span class="material-icons-outlined">monetization_on</span>Sales
-                </a>
-            </li>
-            <li class="sidebar-list-item">
-                <a href="staff-admin.php">
-                <span class="material-icons-outlined">manage_accounts</span>Staff
-                </a>
-            </li>
-        </ul>
-    </aside>
+    <?php include_once "adminHeader.php";?>
     <!-- End Sidebar -->
   
     <main class="main-container">
--- Web/src/Agency/adminHeader.php
+++ Web/src/Agency/adminHeader.php
@@ -1,32 +1,4 @@
-    
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles1.css">
-</head>
-<body>
-  
 
-    <div class="grid-container">
-    <!-- Header -->
-    <header class="header"></header>
-    <!-- <div class="menu-icon" onclick="openSidebar()">
-        <span class="material-icons-outlined">menu</span>
-    </div> -->
-        <div class="header-right">
-            <a href="admin.php">
-        <span class="material-icons-outlined">home</span>
-        </a>
-        <span class="material-icons-outlined">logout</span>
-
-    </div>
-    <!-- End Header -->
-
-    <!-- Sidebar -->
     <aside id="sidebar">
         <div class="sidebar-title">
             <div class="sidebar-brand">
@@ -47,12 +19,7 @@
                 <span class="material-icons-outlined">place</span>Destinations
                 </a>
             </li>
-            <li class="sidebar-list-item">
-                <a href="tours-admin.php">
-                <span class="material-icons-outlined">luggage</span>Review
-                </a>
-            </li>
-            <li class="sidebar-list-item">
+                        <li class="sidebar-list-item">
                 <a href="clients-admin.php">
                 <span class="material-icons-outlined">people</span>Clients
                 </a>
@@ -70,5 +37,3 @@
         </ul>
     </aside>
 
-</body>
-</html>
